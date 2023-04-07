952cb8edbd8fc4a28a5083f359c2b5c1afad8c78
Ishaan Marjit <ishaan_marjit@persistent.com>
2023-04-07 19:08:54 +0530

    hashing

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,102 @@
+﻿using JwtApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace JwtApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+
+            if (user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+            }
+
+            return NotFound("User not found");
+        }
+
+        private string Generate(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Email, user.EmailAddress),
+                new Claim(ClaimTypes.GivenName, user.GivenName),
+                new Claim(ClaimTypes.Surname, user.Surname),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Audience"],
+              claims,
+              expires: DateTime.Now.AddMinutes(15),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        
+        private UserModel Authenticate(UserLogin userLogin)
+{
+    SqlConnection con = new SqlConnection(_config.GetConnectionString("DbConn").ToString());
+    con.Open();
+    String query = "(Select * from users where Username='" + userLogin.Username + "')";
+    SqlDataAdapter da = new SqlDataAdapter(query, con);
+    DataTable dt = new DataTable();
+    da.Fill(dt);
+
+    if (dt.Rows.Count != 0)
+    {
+        DataRow row = dt.Rows[0];
+        string hashedPassword = row["Password"].ToString();
+        bool passwordVerified = BCrypt.Net.BCrypt.Verify(userLogin.Password, hashedPassword);
+
+        if (passwordVerified)
+        {
+            UserModel userModel = new UserModel();
+            userModel.Username = row["Username"].ToString();
+            userModel.EmailAddress = row["EmailAddress"].ToString();
+            userModel.Role = row["Role"].ToString();
+            userModel.Surname = row["Surname"].ToString();
+            userModel.GivenName = row["GivenName"].ToString();
+            return userModel;
+        }
+    }
+
+    return null;
+}
+
+
+    }
+}
