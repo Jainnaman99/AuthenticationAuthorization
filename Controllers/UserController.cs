fc3d9a659c0aaf441b09ee85099465d84b2c55aa
Jainnaman99 <jainnaman99@gmail.com>
2023-04-10 15:10:24 +0530

    rsa module

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,7 +8,9 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
 
 namespace JwtApp.Controllers
 {
@@ -54,10 +56,44 @@ namespace JwtApp.Controllers
         private UserModel GetCurrentUser()
         {
             var identity = HttpContext.User.Identity as ClaimsIdentity;
-
-            if (identity != null)
+            
+            var jwtToken = identity?.FindFirst("jwt")?.Value;
+            //var authorizationHeader = HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+            //var jwtToken = authorizationHeader?.Replace("Bearer ", "");
+           
+            if (jwtToken !=null)
             {
-                var userClaims = identity.Claims;
+                Console.WriteLine(jwtToken);
+                // Create an instance of the JwtSecurityTokenHandler class
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                // Create an instance of the RsaSecurityKey class using the RSA public key
+                //var rsaPublicKey = RSA.Create();
+                //var rsa = RSA.Create();
+                //var rsaPublicKeyBytes = Convert.FromBase64String("your_rsa_public_key_here");
+                //rsaPublicKey.ImportSubjectPublicKeyInfo(rsaPublicKeyBytes, out _);
+                //var rsaKey = new RsaSecurityKey(rsaPublicKey);
+                //var rsa = RSA.Create();
+                using RSA rsa = RSA.Create();
+                RSAParameters rsaPrivateKey = rsa.ExportParameters(false);
+                
+                //var publicKeyParams = rsa.ExportRSAPublicKey();
+                //var rsaPublicKey = RSA.Create();
+                //rsaPublicKey.ImportRSAPublicKey(publicKeyParams, out _);
+                var rsaKey = new RsaSecurityKey(rsaPrivateKey);
+                
+
+                // Create an instance of the TokenValidationParameters class
+                var validationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = rsaKey,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+                 var principal = tokenHandler.ValidateToken(jwtToken, validationParameters, out var validatedToken);
+                 var userName = principal.FindFirst(ClaimTypes.Name)?.Value;
+                 var userClaims = identity.Claims;
 
                 return new UserModel
                 {
@@ -68,6 +104,7 @@ namespace JwtApp.Controllers
                     Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
                 };
             }
+            
             return null;
         }
     }
